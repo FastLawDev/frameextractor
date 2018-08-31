b42d987be103afaa8449d6ea71874501ffb9897b
Vadim Polulyakh <bavadim@gmail.com>
2018-08-31 13:10:41 +0300

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,14 @@ ReactDOM.render(
   <Provider store={store}>
     <div className="container-fluid">
         <div className="row">
+                <div className="col-md-12">
+                        <div className="jumbotron">
+                          <h1 className="display-4">Hello, world!</h1>
+                                <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                        </div>
+                </div>
+        </div>
+        <div className="row">
                 <div className="col-md-8">
                         <DocumentView />
                 </div>
--- src/store/document/types.ts
+++ src/store/document/types.ts
@@ -1,26 +1,80 @@
-export class SelectedItem {
+import { ActionCreator, Action } from 'redux';
+
+/*export class SelectedItem {
         start: number;
         end: number;
         tag: string;
         color: string;
         parNum: number;
+        }*/
+
+export enum SlotType {
+        ORG = "ORG",
+        PERSON = "PERSON",
+        ADDR = "ADDR",
+        EVENT = "EVENT",
+        ACTION = "ACTION"
 }
 
-export enum TypeTag {
-    ORG = "ORG",
-    PERSON = "PERSON",
-    ADDR = "ADDR",
-    EVENT = "EVENT",
-    ACTION = "ACTION"
+export class Slot {
+        name: string;
+        instructions: string;
+        type: SlotType;
+        value: string?;
 }
 
-export interface CurrentDocument {
-  tokens: string[][];
-  selectedTokens: SelectedItem[];
-  currentTag: TypeTag; 
+export class Frame {
+        name: string;
+        slots: Slot[];
 }
 
-import { Action } from 'redux';
+class NullFrame extends Frame {
+        constructor() {
+                this.name = "Больше задач нет!";
+                this.slots = [];
+        }
+}
+
+const nullFrame = new NullFrame();
+
+export class ExtractorState {
+        document: string[][];
+        private emptyFrames: Frame[];
+        private filledFrmaes: Frame[];
+
+        constructor() {
+                this.document = [];
+                this.emptyFrames = [];
+                this.filledFrames = [];
+        }
+
+        private currentFrame(): Frame {
+                if (this.emptyFrames && this.emptyFrames.length > 0) {
+                        return this.emptyFrames[0];
+                } else {
+                        return nullFrame;
+                }
+        }
+
+        currentFrameName(): string {
+                currentFrame().name;
+        }
+
+        currentSlot(): Slot? {
+                const frame = currentFrame();
+                if (frame && frame.slots.length > 0) {
+                        return frame.slots[0]    
+                } else {
+                        return null;
+                }
+        }
+}
+
+export const initialState: CurrentDocument = {
+        tokens: [],
+        selectedTokens: [],
+        currentTag: TypeTag.ORG
+};
 
 export interface TokenClickedAction extends Action {
   type: '@@document/TOKEN_CLICKED';
@@ -44,21 +98,26 @@ export interface TagChangedAction extends Action {
   };
 }
 
-export const initialState: CurrentDocument = {
-  tokens: [],
-  selectedTokens: [],
-  currentTag: TypeTag.ORG
-};
-
-
-export interface Slot {
-        type: TypeTag;
-        value: string;
-}
-
-export interface Frame {
-        type: string;
-        slots: Slot[];
-}
-
 export type DocumentActions = TokenClickedAction | DocumentLoadedAction | TagChangedAction;
+
+export const clickOnToken: ActionCreator<TokenClickedAction> = (selection: SelectedItem[], parNum: number) => ({
+        type: '@@document/TOKEN_CLICKED',
+        payload: {
+                selection,
+                parNum
+        }
+});
+
+export const newDocumentToken: ActionCreator<DocumentLoadedAction> = (tokens: string[][], html: string) => ({
+        type: '@@document/DOCUMENT_LOADED',
+        payload: {
+                tokens
+        }
+});
+
+export const tagChanged: ActionCreator<TagChangedAction> = (tag: TypeTag) => ({
+        type: '@@document/TAG_CHANGED',
+        payload: {
+                tag
+        }
+});
